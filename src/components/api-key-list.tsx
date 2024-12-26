bf67af1162b5c8760400a6417ccc07b093852827
EmmanuelMr18 <emm_mr@hotmail.com>
2024-12-26 14:09:38 -0600

    feat(api-keys): display api-keys and filter by search
    I'm using an infinity scroll just because is easier to implement and
    we already have examples about how to do it.

---
+++ src/components/api-key-list.tsx
@@ -0,0 +1,393 @@
+import { useInfiniteScroll } from "@/hooks/use-infinite-scroll";
+import { useAPIKeyList } from "@/hooks/use-user-settings";
+import { api } from "@/lib/api";
+import { getRelativeTime } from "@/lib/get-relative-time";
+import { zodResolver } from "@hookform/resolvers/zod";
+import type {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+} from "@tanstack/react-table";
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { mutate } from "swr";
+import { useDebounce } from "use-debounce";
+import { z } from "zod";
+import { CopyButton } from "./copy-button";
+import { LoadingIcon } from "./loading-icon";
+import { PaginationControl } from "./paginations-control";
+import { Button } from "./ui/button";
+import { Checkbox } from "./ui/checkbox";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { ScrollArea } from "./ui/scroll-area";
+import { Skeleton } from "./ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+
+export type APIKey = {
+  id: string;
+  key: string;
+  name: string;
+  user_id: string;
+  org_id: string | null;
+  revoked: boolean;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export const columns: ColumnDef<APIKey>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <button type="button" className="flex items-center ">
+          Name
+        </button>
+      );
+    },
+    cell: ({ row }) => {
+      return <span className="ml-3">{row.getValue("name")}</span>;
+    },
+    enableSorting: false,
+  },
+  {
+    accessorKey: "endpoint",
+    header: () => <div className="text-left">Key</div>,
+    cell: ({ row }) => {
+      return <div className="text-left font-medium">{row.original.key}</div>;
+    },
+  },
+  {
+    accessorKey: "date",
+    enableSorting: false,
+    header: ({ column }) => {
+      return (
+        <button type="button" className="flex w-full items-center justify-end ">
+          Update Date
+        </button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-right capitalize">
+        {getRelativeTime(row.original.updated_at)}
+      </div>
+    ),
+  },
+
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const workflow = row.original;
+      // const key = usePaginationMutateKey("api-keys");
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              className="text-destructive"
+              onClick={async () => {
+                // await callServerPromise(deleteAPIKey(workflow.id));
+                // mutate(key);
+              }}
+            >
+              Delete API Key
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+export function APIKeyList() {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
+  const [searchValue, setSearchValue] = useState<string | null>(null);
+  const [debouncedSearchValue] = useDebounce(searchValue, 250);
+
+  const { data, refetch, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useAPIKeyList(debouncedSearchValue ?? "");
+
+  const parentRef = useRef<HTMLDivElement>(null);
+  useInfiniteScroll(parentRef, fetchNextPage, hasNextPage, isFetchingNextPage);
+
+  const flatData = useMemo(() => data?.pages.flat() ?? [], [data]);
+  console.log(flatData);
+
+  useEffect(() => {
+    refetch();
+  }, [debouncedSearchValue]);
+
+  console.log(data);
+  const table = useReactTable({
+    data: flatData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    manualSorting: true,
+    state: {
+      sorting,
+    },
+  });
+
+  return (
+    <div className="h-full w-full px-2 pb-4 md:px-10">
+      <div className="mx-auto grid h-full max-h-[90%] grid-rows-[auto,1fr,auto]">
+        <div className="flex items-center gap-2 py-4">
+          <Input
+            placeholder="Filter API keys..."
+            value={searchValue ?? ""}
+            onChange={(event) => {
+              if (event.target.value === "") {
+                setSearchValue(null);
+              } else {
+                setSearchValue(event.target.value);
+              }
+            }}
+            className="max-w-sm"
+          />
+          <div className="ml-auto flex gap-2">{/* <AddAPIKeyDialog /> */}</div>
+        </div>
+        <ScrollArea
+          className="w-full overflow-x-auto rounded-md border"
+          ref={parentRef}
+        >
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext(),
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => {
+                  console.log(row.getVisibleCells());
+                  return (
+                    <TableRow
+                      key={row.id}
+                      data-state={row.getIsSelected() && "selected"}
+                    >
+                      {row.getVisibleCells().map((cell) => {
+                        return (
+                          <TableCell key={cell.id}>
+                            {cell.getContext().cell.row.original.id &&
+                            cell.getContext().cell.row.original.id.length >
+                              0 ? (
+                              flexRender(
+                                cell.column.columnDef.cell,
+                                cell.getContext(),
+                              )
+                            ) : (
+                              <Skeleton
+                                className="my-2 h-4 w-44"
+                                style={{
+                                  width: cell.column.columnDef.size,
+                                }}
+                              />
+                            )}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </ScrollArea>
+      </div>
+    </div>
+  );
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+// function AddAPIKeyDialog() {
+//   const [open, setOpen] = useState(false);
+//   const form = useForm<z.infer<typeof formSchema>>({
+//     resolver: zodResolver(formSchema),
+//     defaultValues: {
+//       name: "My API Key",
+//     },
+//   });
+
+//   const [apiKey, setAPIKey] = useState<Awaited<
+//     ReturnType<typeof addNewAPIKey>
+//   > | null>(null);
+
+//   const key = usePaginationMutateKey("api-keys");
+
+//   const handleOpenChange = (open: boolean) => {
+//     setOpen(open);
+//     if (!open) {
+//       setAPIKey(null);
+//       form.reset({ name: "My API Key" });
+//     }
+//   };
+
+//   return (
+//     <Dialog open={open} onOpenChange={handleOpenChange}>
+//       <DialogTrigger asChild>
+//         <Button variant="default" className="">
+//           Create API Key
+//         </Button>
+//       </DialogTrigger>
+//       <DialogContent className="sm:max-w-[425px]">
+//         <Form {...form}>
+//           <form
+//             onSubmit={form.handleSubmit(async (data) => {
+//               const apiKey = await callServerPromise(addNewAPIKey(data.name));
+//               if (apiKey) setAPIKey(apiKey);
+//               mutate(key);
+//               // setOpen(false);
+//             })}
+//           >
+//             <DialogHeader>
+//               <DialogTitle>Create API Key</DialogTitle>
+//               <DialogDescription>
+//                 Create API Key for workflow upload
+//               </DialogDescription>
+//             </DialogHeader>
+//             <div className="grid gap-4 py-4">
+//               {/* <div className="grid grid-cols-4 items-center gap-4"> */}
+//               <FormField
+//                 control={form.control}
+//                 name="name"
+//                 render={({ field }) => (
+//                   <FormItem>
+//                     <FormLabel>Name</FormLabel>
+//                     <FormControl>
+//                       <Input {...field} />
+//                     </FormControl>
+//                     <FormMessage />
+//                   </FormItem>
+//                 )}
+//               />
+//               {apiKey && (
+//                 <FormItem>
+//                   <FormLabel>API Key (Copy the API key now)</FormLabel>
+//                   <div className="flex gap-2">
+//                     <FormControl>
+//                       <Input readOnly value={apiKey.key} />
+//                     </FormControl>
+//                     <CopyButton text={apiKey.key} className="" />
+//                   </div>
+//                 </FormItem>
+//               )}
+//             </div>
+//             <DialogFooter>
+//               {apiKey ? (
+//                 <Button
+//                   className="flex gap-2"
+//                   type="button"
+//                   onClick={(e) => {
+//                     e.preventDefault();
+//                     setOpen(false);
+//                     setAPIKey(null);
+//                     form.reset({ name: "My API Key" });
+//                   }}
+//                 >
+//                   Close {form.formState.isSubmitting && <LoadingIcon />}
+//                 </Button>
+//               ) : (
+//                 <Button
+//                   className="flex gap-2"
+//                   type="submit"
+//                   disabled={form.formState.isSubmitting}
+//                 >
+//                   Create {form.formState.isSubmitting && <LoadingIcon />}
+//                 </Button>
+//               )}
+//             </DialogFooter>
+//           </form>
+//         </Form>
+//       </DialogContent>
+//     </Dialog>
+//   );
+// }
+
+// type GetAPIKeysParams = {
+//   search?: string;
+//   limit?: number;
+//   offset?: number;
+// };
+
+// async function getAPIKeys(params: Partial<GetAPIKeysParams>) {
+//   const response = await api({
+//     url: "platform/api-keys",
+//     params,
+//   });
+//   return response;
+// }
--- src/hooks/use-user-settings.ts
+++ src/hooks/use-user-settings.ts
@@ -1,7 +1,34 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 export function useUserSettings() {
   return useQuery<any>({
     queryKey: ["platform", "user-settings"],
+  });
+}
+
+const BATCH_SIZE = 20;
+
+export function useAPIKeyList(debouncedSearchValue: string) {
+  return useInfiniteQuery<any[]>({
+    queryKey: ["platform", "api-keys"],
+    meta: {
+      limit: BATCH_SIZE,
+      offset: 0,
+      params: {
+        search: debouncedSearchValue ?? "",
+      },
+    },
+    getNextPageParam: (lastPage, allPages) => {
+      // Check if lastPage is defined and has a length property
+      if (
+        lastPage &&
+        Array.isArray(lastPage) &&
+        lastPage.length === BATCH_SIZE
+      ) {
+        return allPages.length * BATCH_SIZE;
+      }
+      return undefined;
+    },
+    initialPageParam: 0,
   });
 }
