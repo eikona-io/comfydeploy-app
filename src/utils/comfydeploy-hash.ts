9e59a4e6c733079ad3e9bc6f03f3268b39d8e8a9
bennykok <itechbenny@gmail.com>
2025-01-05 17:40:40 +0800

    fix: bump hash

--- src/utils/comfydeploy-hash.ts
+++ src/utils/comfydeploy-hash.ts
@@ -1,3 +1,3 @@
-export const comfydeploy_hash = "ac843527d9b28e12a98803ab5f99a80da677545d";
+export const comfydeploy_hash = "5423b4ee6f5d1bb0e9a84b6fc84bdc0f2814c825";
 
 export const comfyui_hash = "8ce2a1052ca03183768da0aaa483024e58b8008c";
