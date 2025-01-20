511335e34df045611ccef05403a1e7005414fcac
karrix <thomasdice1834793903@gmail.com>
2025-01-20 20:02:40 +0800

    fix: file size display

--- src/components/storage/FileTable.tsx
+++ src/components/storage/FileTable.tsx
@@ -196,13 +196,13 @@ export const columns: ColumnDef<EnhancedFileEntry>[] = [
 export function formatFileSize(fileSize: number) {
   let sizeString;
   if (fileSize >= 1073741824) {
-    sizeString = (fileSize / 1073741824).toFixed(2) + " GB";
+    sizeString = `${(fileSize / 1073741824).toFixed(2)} GB`;
   } else if (fileSize >= 1048576) {
-    sizeString = (fileSize / 1048576).toFixed(2) + " MB";
+    sizeString = `${(fileSize / 1048576).toFixed(2)} MB`;
   } else if (fileSize >= 1024) {
-    sizeString = (fileSize / 1024).toFixed(2) + " KB";
+    sizeString = `${(fileSize / 1024).toFixed(2)} KB`;
   } else {
-    sizeString = fileSize + " bytes";
+    sizeString = `${fileSize} bytes`;
   }
   return sizeString;
 }
