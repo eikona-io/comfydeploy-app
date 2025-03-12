426e7e4fcfc84a6a21dfdb9add6c116193a0c398
bennykok <itechbenny@gmail.com>
2025-03-12 23:12:33 +0800

    update comfyui, comfydeploy hash
    # Conflicts:
    #	src/utils/comfydeploy-hash.ts

--- src/utils/comfydeploy-hash.ts
+++ src/utils/comfydeploy-hash.ts
@@ -1,3 +1,3 @@
-export const comfydeploy_hash = "ce3b0dbe849ed58285adfca49d42a7a1e3370589";
+export const comfydeploy_hash = "6ecf9c782b92cb612f69d36f80081e7bd8c0d6b0";
 
-export const comfyui_hash = "8ce2a1052ca03183768da0aaa483024e58b8008c";
+export const comfyui_hash = "9aac21f894a122ddb8d825c57ad61c0db5e630db";
