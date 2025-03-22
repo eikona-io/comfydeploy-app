032b55a88f169466350899c83f564a7c979fa7ec
karrix <thomasdice1834793903@gmail.com>
2025-03-22 15:54:29 +0800

    refactor

--- src/components/deployment/deployment-page.tsx
+++ src/components/deployment/deployment-page.tsx
@@ -85,62 +85,8 @@ export const useSelectedDeploymentStore = create<SelectedDeploymentState>(
 
 export function DeploymentPage() {
   const { workflowId } = useParams({ from: "/workflows/$workflowId/$view" });
-  const { workflow } = useCurrentWorkflow(workflowId);
-  const { data: machine } = useMachine(workflow?.selected_machine_id);
-  const {
-    data: deployments,
-    refetch: refetchDeployments,
-    isLoading: isDeploymentsLoading,
-  } = useWorkflowDeployments(workflowId);
-  const { data: versions } = useQuery<Version[]>({
-    queryKey: ["workflow", workflowId, "versions"],
-    meta: {
-      params: {
-        limit: 1,
-        offset: 0,
-      },
-    },
-  });
-  const { setSelectedDeployment } = useSelectedDeploymentStore();
-  const [isPromoting, setIsPromoting] = useState(false);
-
-  const handlePromoteToEnv = async ({
-    environment,
-    workflowVersionId,
-    machineId,
-    machineVersionId,
-  }: {
-    environment: "production" | "staging";
-    workflowVersionId: string;
-    machineId: string;
-    machineVersionId: string;
-  }) => {
-    try {
-      setIsPromoting(true);
-      const deployment = await callServerPromise(
-        api({
-          url: "deployment",
-          init: {
-            method: "POST",
-            body: JSON.stringify({
-              workflow_id: workflowId,
-              workflow_version_id: workflowVersionId,
-              machine_id: machineId,
-              machine_version_id: machineVersionId,
-              environment,
-            }),
-          },
-        }),
-      );
-      refetchDeployments();
-      setSelectedDeployment(deployment.id);
-      toast.success("Deployment promoted successfully");
-    } catch (error) {
-      toast.error("Failed to promote deployment");
-    } finally {
-      setIsPromoting(false);
-    }
-  };
+  const { data: deployments, isLoading: isDeploymentsLoading } =
+    useWorkflowDeployments(workflowId);
 
   return (
     <>
@@ -186,165 +132,7 @@ export function DeploymentPage() {
         </div>
 
         <h3 className="mt-4 mb-2 ml-2 font-medium text-sm">Versions</h3>
-        <VersionList
-          hideSearch
-          workflow_id={workflowId || ""}
-          className="relative z-[1] w-full rounded-md bg-background p-1 shadow-sm ring-1 ring-gray-200"
-          containerClassName="max-h-[234px]"
-          height={30}
-          renderItem={(item: Version) => {
-            const myDeployments = deployments?.filter(
-              (deployment: Deployment) =>
-                deployment.workflow_version_id === item.id,
-            );
-
-            return (
-              <div
-                className={cn(
-                  "flex flex-row items-center justify-between gap-2 rounded-[6px] px-4 transition-colors hover:bg-gray-100",
-                  // isSelected && "bg-gray-200 hover:bg-gray-200",
-                  item.version === 1 && "rounded-b-sm",
-                  item.version === versions?.[versions.length - 1]?.version &&
-                    "rounded-t-sm",
-                )}
-              >
-                <div className="grid grid-cols-[14px_38px_auto_1fr] items-center gap-4">
-                  <div className="flex h-full items-center justify-center">
-                    {versions && (
-                      <>
-                        <div
-                          className={cn(
-                            "absolute w-[2px] bg-gray-300",
-                            item.version === 1
-                              ? "top-0 h-[50%]"
-                              : item.version === versions?.[0]?.version
-                                ? "bottom-0 h-[50%]"
-                                : "h-full",
-                          )}
-                        />
-                        <div className="relative z-10 flex h-[6px] w-[6px] items-center justify-center rounded-full bg-gray-300" />
-                      </>
-                    )}
-                  </div>
-
-                  <Badge className="!py-0 !text-2xs w-fit whitespace-nowrap rounded-sm">
-                    v{item.version}
-                  </Badge>
-
-                  <div className="truncate text-muted-foreground text-xs">
-                    {item.comment}
-                  </div>
-                </div>
-                <div className="grid grid-cols-[auto_auto_110px_30px] items-center gap-4">
-                  {myDeployments?.length > 0 ? (
-                    <div className="flex flex-row gap-2">
-                      {myDeployments
-                        .filter(
-                          (deployment: Deployment) =>
-                            deployment.environment === "production" ||
-                            deployment.environment === "staging",
-                        )
-                        .map((deployment: Deployment) => (
-                          <Badge
-                            key={deployment.id}
-                            className={cn(
-                              "!text-2xs w-fit cursor-pointer whitespace-nowrap rounded-md hover:shadow-sm",
-                              getEnvColor(deployment.environment),
-                            )}
-                            onClick={(e) => {
-                              e.preventDefault();
-                              e.stopPropagation();
-                              e.nativeEvent.preventDefault();
-                              e.nativeEvent.stopPropagation();
-
-                              setSelectedDeployment(deployment.id);
-                            }}
-                          >
-                            {deployment.environment}
-                          </Badge>
-                        ))}
-                    </div>
-                  ) : (
-                    <div />
-                  )}
-                  <UserIcon user_id={item.user_id} className="h-5 w-5" />
-                  <div className="whitespace-nowrap text-2xs text-muted-foreground">
-                    {getRelativeTime(item.created_at)}
-                  </div>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger
-                      asChild
-                      className="h-full w-full rounded-sm p-2 hover:bg-gray-50"
-                      onClick={(e) => e.stopPropagation()} // Prevent triggering the row click
-                    >
-                      <MoreVertical size={16} />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-56 overflow-visible">
-                      <DropdownMenuItem
-                        className="p-0"
-                        disabled={isPromoting}
-                        onClick={async (e) => {
-                          e.preventDefault();
-                          e.stopPropagation();
-                          e.nativeEvent.preventDefault();
-                          e.nativeEvent.stopPropagation();
-                          await handlePromoteToEnv({
-                            environment: "production",
-                            workflowVersionId: item.id,
-                            machineId: machine?.id,
-                            machineVersionId: machine?.machine_version_id,
-                          });
-                        }}
-                      >
-                        <Button
-                          variant={"ghost"}
-                          className="w-full justify-between gap-2 px-2 font-normal"
-                        >
-                          <div className="flex flex-row gap-2">
-                            Promote to
-                            <Badge variant="blue">production</Badge>
-                          </div>
-                          {isPromoting && (
-                            <Loader2 className="h-4 w-4 animate-spin" />
-                          )}
-                        </Button>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        className="p-0"
-                        disabled={isPromoting}
-                        onClick={async (e) => {
-                          e.preventDefault();
-                          e.stopPropagation();
-                          e.nativeEvent.preventDefault();
-                          e.nativeEvent.stopPropagation();
-                          await handlePromoteToEnv({
-                            environment: "staging",
-                            workflowVersionId: item.id,
-                            machineId: machine?.id,
-                            machineVersionId: machine?.machine_version_id,
-                          });
-                        }}
-                      >
-                        <Button
-                          variant={"ghost"}
-                          className="w-full justify-between gap-2 px-2 font-normal"
-                        >
-                          <div className="flex flex-row gap-2">
-                            Promote to
-                            <Badge variant="yellow">staging</Badge>
-                          </div>
-                          {isPromoting && (
-                            <Loader2 className="h-4 w-4 animate-spin" />
-                          )}
-                        </Button>
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </div>
-              </div>
-            );
-          }}
-        />
+        <DeploymentWorkflowVersionList workflowId={workflowId} />
       </div>
       <DeploymentDrawer />
     </>
@@ -425,5 +213,222 @@ function DeploymentHistory({ deployment }: { deployment: Deployment }) {
         </TooltipProvider>
       </div>
     </div>
+  );
+}
+
+function DeploymentWorkflowVersionList({ workflowId }: { workflowId: string }) {
+  const { workflow } = useCurrentWorkflow(workflowId);
+  const { data: machine } = useMachine(workflow?.selected_machine_id);
+  const { data: deployments, refetch: refetchDeployments } =
+    useWorkflowDeployments(workflowId);
+  const { data: versions } = useQuery<Version[]>({
+    queryKey: ["workflow", workflowId, "versions"],
+    meta: {
+      params: {
+        limit: 1,
+        offset: 0,
+      },
+    },
+  });
+  const { setSelectedDeployment } = useSelectedDeploymentStore();
+  const [isPromoting, setIsPromoting] = useState(false);
+
+  const handlePromoteToEnv = async ({
+    environment,
+    workflowVersionId,
+    machineId,
+    machineVersionId,
+  }: {
+    environment: "production" | "staging";
+    workflowVersionId: string;
+    machineId: string;
+    machineVersionId: string;
+  }) => {
+    try {
+      setIsPromoting(true);
+      const deployment = await callServerPromise(
+        api({
+          url: "deployment",
+          init: {
+            method: "POST",
+            body: JSON.stringify({
+              workflow_id: workflowId,
+              workflow_version_id: workflowVersionId,
+              machine_id: machineId,
+              machine_version_id: machineVersionId,
+              environment,
+            }),
+          },
+        }),
+      );
+      refetchDeployments();
+      setSelectedDeployment(deployment.id);
+      toast.success("Deployment promoted successfully");
+    } catch (error) {
+      toast.error("Failed to promote deployment");
+    } finally {
+      setIsPromoting(false);
+    }
+  };
+
+  return (
+    <VersionList
+      hideSearch
+      workflow_id={workflowId || ""}
+      className="relative z-[1] w-full rounded-md bg-background p-1 shadow-sm ring-1 ring-gray-200"
+      containerClassName="max-h-[234px]"
+      height={30}
+      renderItem={(item: Version) => {
+        const myDeployments = deployments?.filter(
+          (deployment: Deployment) =>
+            deployment.workflow_version_id === item.id,
+        );
+
+        return (
+          <div
+            className={cn(
+              "flex flex-row items-center justify-between gap-2 rounded-[6px] px-4 transition-colors hover:bg-gray-100",
+              item.version === 1 && "rounded-b-sm",
+              item.version === versions?.[versions.length - 1]?.version &&
+                "rounded-t-sm",
+            )}
+          >
+            <div className="grid grid-cols-[14px_38px_auto_1fr] items-center gap-4">
+              <div className="flex h-full items-center justify-center">
+                {versions && (
+                  <>
+                    <div
+                      className={cn(
+                        "absolute w-[2px] bg-gray-300",
+                        item.version === 1
+                          ? "top-0 h-[50%]"
+                          : item.version === versions?.[0]?.version
+                            ? "bottom-0 h-[50%]"
+                            : "h-full",
+                      )}
+                    />
+                    <div className="relative z-10 flex h-[6px] w-[6px] items-center justify-center rounded-full bg-gray-300" />
+                  </>
+                )}
+              </div>
+
+              <Badge className="!py-0 !text-2xs w-fit whitespace-nowrap rounded-sm">
+                v{item.version}
+              </Badge>
+
+              <div className="truncate text-muted-foreground text-xs">
+                {item.comment}
+              </div>
+            </div>
+            <div className="grid grid-cols-[auto_auto_110px_30px] items-center gap-4">
+              {myDeployments?.length > 0 ? (
+                <div className="flex flex-row gap-2">
+                  {myDeployments
+                    .filter(
+                      (deployment: Deployment) =>
+                        deployment.environment === "production" ||
+                        deployment.environment === "staging",
+                    )
+                    .map((deployment: Deployment) => (
+                      <Badge
+                        key={deployment.id}
+                        className={cn(
+                          "!text-2xs w-fit cursor-pointer whitespace-nowrap rounded-md hover:shadow-sm",
+                          getEnvColor(deployment.environment),
+                        )}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          e.stopPropagation();
+                          e.nativeEvent.preventDefault();
+                          e.nativeEvent.stopPropagation();
+
+                          setSelectedDeployment(deployment.id);
+                        }}
+                      >
+                        {deployment.environment}
+                      </Badge>
+                    ))}
+                </div>
+              ) : (
+                <div />
+              )}
+              <UserIcon user_id={item.user_id} className="h-5 w-5" />
+              <div className="whitespace-nowrap text-2xs text-muted-foreground">
+                {getRelativeTime(item.created_at)}
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger
+                  asChild
+                  className="h-full w-full rounded-sm p-2 hover:bg-gray-50"
+                  onClick={(e) => e.stopPropagation()} // Prevent triggering the row click
+                >
+                  <MoreVertical size={16} />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56 overflow-visible">
+                  <DropdownMenuItem
+                    className="p-0"
+                    disabled={isPromoting}
+                    onClick={async (e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      e.nativeEvent.preventDefault();
+                      e.nativeEvent.stopPropagation();
+                      await handlePromoteToEnv({
+                        environment: "production",
+                        workflowVersionId: item.id,
+                        machineId: machine?.id,
+                        machineVersionId: machine?.machine_version_id,
+                      });
+                    }}
+                  >
+                    <Button
+                      variant={"ghost"}
+                      className="w-full justify-between gap-2 px-2 font-normal"
+                    >
+                      <div className="flex flex-row gap-2">
+                        Promote to
+                        <Badge variant="blue">production</Badge>
+                      </div>
+                      {isPromoting && (
+                        <Loader2 className="h-4 w-4 animate-spin" />
+                      )}
+                    </Button>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="p-0"
+                    disabled={isPromoting}
+                    onClick={async (e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      e.nativeEvent.preventDefault();
+                      e.nativeEvent.stopPropagation();
+                      await handlePromoteToEnv({
+                        environment: "staging",
+                        workflowVersionId: item.id,
+                        machineId: machine?.id,
+                        machineVersionId: machine?.machine_version_id,
+                      });
+                    }}
+                  >
+                    <Button
+                      variant={"ghost"}
+                      className="w-full justify-between gap-2 px-2 font-normal"
+                    >
+                      <div className="flex flex-row gap-2">
+                        Promote to
+                        <Badge variant="yellow">staging</Badge>
+                      </div>
+                      {isPromoting && (
+                        <Loader2 className="h-4 w-4 animate-spin" />
+                      )}
+                    </Button>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        );
+      }}
+    />
   );
 }
