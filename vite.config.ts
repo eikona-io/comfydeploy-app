44702151108eaa346c641114cd474c9bb0ab00bf
karrix <thomasdice1834793903@gmail.com>
2025-03-29 02:11:12 +0100

    feat: vite auto update route

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 // import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -27,7 +28,7 @@ export default defineConfig(({ command, mode }) => {
   );
 
   return {
-    plugins: [react()],
+    plugins: [TanStackRouterVite(), react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
