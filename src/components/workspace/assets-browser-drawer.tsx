aa2fdb4263d8bee95d2628bccd90daa7f6711ca8
karrix <thomasdice1834793903@gmail.com>
2025-04-09 23:22:00 +0300

    feat: assets input

--- src/components/workspace/assets-browser-drawer.tsx
+++ src/components/workspace/assets-browser-drawer.tsx
@@ -1,18 +1,18 @@
-"use client";
-
 import { cn } from "@/lib/utils";
-// import { PreventNavigation } from "@/repo/components/ui/custom/prevent-navigation";
-// import { createNewDraftVersion } from "@/server/actions/cdActions";
-// import { uploadFile } from "@repo/lib/uploadFile";
 import { sendEventToCD } from "./sendEventToCD";
-
-// import { usePathname, useRouter } from "next/navigation";
 import { useMediaQuery } from "usehooks-ts";
 import { Drawer } from "vaul";
 import { AssetBrowser } from "../asset-browser";
 import { useAssetsBrowserStore } from "./Workspace";
+import type { AssetType } from "../SDInputs/sd-asset-input";
 
-export function AssetsBrowserPopup() {
+export function AssetsBrowserPopup({
+  isPlayground,
+  handleAsset,
+}: {
+  isPlayground?: boolean;
+  handleAsset: (asset: AssetType) => void;
+}) {
   const { open, setOpen, targetNodeData } = useAssetsBrowserStore();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
@@ -27,7 +27,7 @@ export function AssetsBrowserPopup() {
           className={cn(
             isMobile
               ? "fixed right-0 bottom-0 left-0 mt-24 flex h-[96%] flex-col rounded-t-[10px] bg-white md:top-0 md:right-0 md:bottom-0 md:h-full md:w-[400px] md:rounded-l-[10px] md:rounded-tr-none"
-              : "fixed top-2 right-2 bottom-2 z-10 flex w-[500px] outline-none",
+              : "fixed top-2 right-2 bottom-2 z-10 flex w-[500px] rounded-[10px] border border-gray-300/50 shadow-lg",
           )}
           style={
             {
@@ -46,6 +46,10 @@ export function AssetsBrowserPopup() {
             </div>
             <AssetBrowser
               onItemClick={(asset) => {
+                if (isPlayground && handleAsset) {
+                  handleAsset(asset);
+                  return;
+                }
                 if (targetNodeData?.node) {
                   // If we have target node data, update the existing node
                   console.log(targetNodeData);
