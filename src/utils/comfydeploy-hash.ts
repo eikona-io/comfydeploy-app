b4697ff1749373142da432d33a4f887aa8d8587a
bennykok <itechbenny@gmail.com>
2025-04-30 16:11:36 +0800

    chore: update comfydeploy_hash to new value

--- src/utils/comfydeploy-hash.ts
+++ src/utils/comfydeploy-hash.ts
@@ -1,3 +1,3 @@
-export const comfydeploy_hash = "f19b80a25d118f9321fae29a1630240375ff29e3";
+export const comfydeploy_hash = "95fc6427820ad4178a66e1f4e07ad39e0f9da352";
 
 export const comfyui_hash = "9aac21f894a122ddb8d825c57ad61c0db5e630db";
