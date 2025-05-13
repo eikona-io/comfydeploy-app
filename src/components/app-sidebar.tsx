5de6177a8e07c1ba3b9ca5f2517d7bc52c637cf7
KarrixLee <thomasdice1834793903@gmail.com>
2025-05-13 17:37:02 +0800

    temp disable analytics

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -267,15 +267,15 @@ function usePages() {
         ]
       : []),
 
-    ...(sub?.plans?.plans
-      ? [
-          {
-            name: "Analytics",
-            path: "/analytics",
-            icon: LineChart,
-          },
-        ]
-      : []),
+    // ...(sub?.plans?.plans
+    //   ? [
+    //       {
+    //         name: "Analytics",
+    //         path: "/analytics",
+    //         icon: LineChart,
+    //       },
+    //     ]
+    //   : []),
     {
       name: "Plan",
       path: "/pricing",
