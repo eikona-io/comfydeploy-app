f07067c25f0e2c125c58546c8c26cdc568f2c400
KarrixLee <thomasdice1834793903@gmail.com>
2025-06-19 17:40:19 +0800

    tweak

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -119,7 +119,7 @@ function RootComponent() {
         <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)] dark:bg-[radial-gradient(#333333_1px,transparent_1px)]" />
       </div>
 
-      {isAuthPage ? (
+      {isAuthPage && !auth.isSignedIn ? (
         <GuestSidebar />
       ) : (
         !isSessionPage && (
