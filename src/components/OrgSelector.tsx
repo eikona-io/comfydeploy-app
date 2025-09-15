4a421e78a9d30ae3d60581272cc87358416db4af
bennykok <itechbenny@gmail.com>
2025-09-15 13:46:39 -0700

    fi

--- src/components/OrgSelector.tsx
+++ src/components/OrgSelector.tsx
@@ -1,13 +1,20 @@
 import {
   CreateOrganization,
   OrganizationList,
+  useAuth,
+  useClerk,
   useOrganization,
   useOrganizationList,
 } from "@clerk/clerk-react";
 import { dark } from "@clerk/themes";
 import { useCustomer } from "autumn-js/react";
+import { useState } from "react";
 import { isDarkTheme } from "@/lib/utils";
 import { useTheme } from "./theme-provider";
+import { Button } from "./ui/button";
+import { LoadingIcon } from "./ui/custom/loading-icon";
+import { Input } from "./ui/input";
+import { LoadingProgress } from "./ui/loading-progress";
 import { Skeleton } from "./ui/skeleton";
 
 export function useOrgSelector() {
@@ -18,66 +25,118 @@ export function useOrgSelector() {
     userMemberships: true,
   });
 
+  const auth = useAuth();
+  const clerk = useClerk();
+
   const { check, isLoading } = useCustomer();
   const workflowLimit = check({ featureId: "workflow_limit" });
 
-  console.log("isLoading", isLoading);
-  console.log("isLoaded", isLoaded);
-  console.log("isOrganizationLoaded", isOrganizationLoaded);
-  console.log("workflowLimit", workflowLimit);
+  const [isCreatingOrg, setIsCreatingOrg] = useState(false);
 
-  // if (isLoading || !isLoaded || !isOrganizationLoaded) {
-  // return (
-  //   <div className="h-full flex w-full flex-col items-center justify-center p-8 bg-background z-50">
-  //     Loading...
-  //   </div>
-  // );
-  // }
+  // console.log("organization", organization);
+  // console.log("isLoading", isLoading);
+  // console.log("isLoaded", isLoaded);
+  // console.log("isOrganizationLoaded", isOrganizationLoaded);
+  // console.log("workflowLimit", workflowLimit);
 
-  //   The user is in a personal org, has other org, no workflows, show the org list
+  if (!auth.isLoaded || !auth.isSignedIn) {
+    return null;
+  }
+
   if (
-    !organization &&
-    userMemberships?.count &&
-    userMemberships.count > 0 &&
-    workflowLimit.data.usage === 0
+    isLoading ||
+    !isLoaded ||
+    !isOrganizationLoaded ||
+    userMemberships.isFetching
   ) {
     return (
-      <div className="h-full flex  w-full flex-col items-center justify-center p-8 bg-background z-50">
-        <div className="text-center">
-          <OrganizationList
-            hidePersonal
-            afterSelectOrganizationUrl="/org/:slug/workflows"
-            appearance={{
-              baseTheme: isDarkTheme(theme) ? dark : undefined,
-              elements: {
-                cardBox: "dark:border-zinc-700",
-              },
-            }}
-          />
+      <>
+        <div className="h-full flex  w-full flex-col items-center justify-center p-8 bg-background z-50">
+          <LoadingIcon />
         </div>
-      </div>
+      </>
     );
   }
 
-  //   The user is in a personal org, no workflows, show the create org form
-  if (!organization && workflowLimit.data.usage === 0) {
-    return (
-      <div className="h-full inset-0 flex flex-col items-center justify-center p-8 bg-background  z-50">
-        <div className="text-center">
-          <CreateOrganization
-            afterCreateOrganizationUrl="/org/:slug/workflows"
-            //   afterCreateOrganizationUrl="/pricing"
-            hideSlug
-            appearance={{
-              baseTheme: isDarkTheme(theme) ? dark : undefined,
-              elements: {
-                cardBox: "dark:border-zinc-700",
-              },
-            }}
-          />
+  //   The user is in a personal org, has other org, no workflows, show the org list
+  if (!organization) {
+    if (
+      userMemberships?.count &&
+      userMemberships.count > 0 &&
+      workflowLimit.data.usage === 0
+    ) {
+      return (
+        <div className="h-full flex  w-full flex-col items-center justify-center p-8 bg-background z-50">
+          <div className="text-center">
+            <OrganizationList
+              hidePersonal
+              afterSelectOrganizationUrl="/org/:slug/workflows"
+              afterCreateOrganizationUrl="/org/:slug/workflows"
+              appearance={{
+                baseTheme: isDarkTheme(theme) ? dark : undefined,
+                elements: {
+                  cardBox: "dark:border-zinc-700",
+                },
+              }}
+            />
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+
+    //   The user is in a personal org, no workflows, show the create org form
+    const dontHaveAnyOrgs = userMemberships?.count === 0;
+    // console.log("dontHaveAnyOrgs", dontHaveAnyOrgs, userMemberships);
+    if (dontHaveAnyOrgs && workflowLimit.data.usage === 0) {
+      const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        setIsCreatingOrg(true);
+        const organization = await clerk.createOrganization({ name });
+        console.log("organization", organization);
+        setIsCreatingOrg(false);
+        window.location.href = `/org/${organization.slug}/workflows`;
+        // router.push(`/org/${organization.id}/workflows`);
+      };
+      return (
+        <div className="h-full inset-0 flex flex-col items-center justify-center p-8 z-50 w-full max-w-md mx-auto">
+          <div className="text-center w-full">
+            <form
+              onSubmit={handleSubmit}
+              className="w-full max-w-md space-y-6 w-full"
+            >
+              <div className="space-y-2">
+                <h1 className="text-2xl font-semibold text-foreground text-left">
+                  Create Organization
+                </h1>
+                <p className="text-sm text-muted-foreground text-left">
+                  Deploy Comfy as API, share as a simplfied playground, access
+                  cloud GPU
+                </p>
+              </div>
+
+              <div className="space-y-2">
+                <Input
+                  id="name"
+                  name="name"
+                  placeholder="Organization name"
+                  className="w-full"
+                  required
+                />
+              </div>
+
+              <Button
+                type="submit"
+                isLoading={isCreatingOrg}
+                className="w-full"
+              >
+                Get Started
+              </Button>
+            </form>
+          </div>
+        </div>
+      );
+    }
   }
 
   //   Legazy user, personal org, has workflows
